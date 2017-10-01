7c0a6b01070bbc08345c508edabe2f379fa6cf39
ThinkerM <filip.milichovsky@gmail.com>
2017-10-01 18:09:17 +0200

    Add CharsAndString Extensions

---
+++ CharsAndStrings/CharExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharsAndStrings
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="char"/>
+    /// </summary>
+    public static class CharExtensions
+    {
+    }
+}
